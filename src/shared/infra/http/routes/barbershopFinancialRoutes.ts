2c0884d23b7e200920f3d35ecb6fd13d69a7f580
ViniciusDanielHub <viniciushubgit@hotmail.com>
2026-06-13 03:09:11 -0300

    feat: Creating a financial plan for the barbershop

---
+++ src/shared/infra/http/routes/barbershopFinancialRoutes.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from "fastify";
+import { authenticate } from "../middlewares/authenticate";
+import { authorize } from "../middlewares/authorize";
+import { checkSubscription } from "../middlewares/checkSubscription";
+import { BarbershopFinancialController } from "../../../../modules/barbershops/controllers/BarbershopFinancialController";
+
+const financial = new BarbershopFinancialController();
+
+export async function barbershopFinancialRoutes(app: FastifyInstance) {
+  const preHandler = [authenticate, authorize(["OWNER"]), checkSubscription];
+
+  app.get("/barbershop/financial/summary", { preHandler }, financial.summary.bind(financial));
+  app.get("/barbershop/financial/expenses", { preHandler }, financial.expenses.bind(financial));
+  app.get("/barbershop/financial/fiados", { preHandler }, financial.fiados.bind(financial));
+}
